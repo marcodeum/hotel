b25bb644642aed1477d86a1ab952d8db3640b26c
Marco Túlio <marco.souza300322@gmail.com>
2026-09-08 22:06:27 -0300

    Autenticação finalizada

---
+++ autenticar.php
@@ -0,0 +1,23 @@
+<?php
+
+include 'conexao.php';
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$sql = "SELECT * FROM clientes WHERE senha = '$senha' AND email = '$email'";
+
+$resultado = mysqli_query(
+    $conexao,
+    $sql
+);
+
+if(mysqli_num_rows($resultado) > 0){
+    echo "Login realizado com sucesso!\n";
+    echo "<a href='reservas.html'>CONTINUAR</a>";
+}else{
+    echo "Email ou senha inválidos! Tente novamente!\n";
+    echo "<a href='login.html'>VOLTAR</a>";
+}
+
+?>
